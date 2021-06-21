a8ff9ea673205cd789e03569247e93da17dfb748
GilMyungChul <42732849+GilMyungChul@users.noreply.github.com>
2021-06-21 17:56:12 +0900

    Update Gpinch.js

--- portfolio/js/Gpinch.js
+++ portfolio/js/Gpinch.js
@@ -1,3 +1,11 @@
+// 'use strict'
+
+/*** 핀치줌 플러그인 제작
+  ====================== ver 1.0 ======================
+    * 2021-06-21
+      - pinch zoom  / hover zoom 완료
+***/
+
 (function($){
 
     var plugin = {};
@@ -6,7 +14,7 @@
     var defaults = {
         mode: 'pinch',                                                  // pinch zoom mode : 'pinch' , 'hover'
         scaleValue: 1.5,                                                // pinch scale Value : 1.5 (default)
-        transition_Duration: 0.5,                                         // pinch transition Duration
+        transition_Duration: 0.5,                                       // pinch transition Duration
 
         swiper: false,                                                  // swiper 여부
     };
@@ -35,13 +43,13 @@
             pinch.setting = $.extend({}, defaults, option);
 
             pinch.evtZoomChk = true;
-
-            pinch.modeValue = pinch.setting.mode == 'pinch' ? 'click' : 'mouseenter';
-
+            
             pinch.evtTypeStart = document.ontouchstart !== null ? 'mousedown' : 'touchstart';                                                                // 모바일, PC 구분 시작 이벤트
             pinch.evtTypeMove = document.ontouchstart !== null ? 'mousemove' : 'touchmove';                                                                  // 모바일, PC 구분 무브 이벤트
             pinch.evtTypeEnd = document.ontouchstart !== null ? 'mouseup' : 'touchend';                                                                      // 모바일, PC 구분 끝 이벤트
-
+            
+            pinch.modeValue = pinch.setting.mode == 'pinch' ? pinch.evtTypeStart : 'mouseenter';
+            
             pinch.container;
             pinch.targetItem = el.find('.pinch-item');
 
@@ -51,7 +59,10 @@
             pinch.imgWidth = pinch.targetImg.width();
             pinch.imgHeight = pinch.targetImg.height();
 
+            pinch.obj;
+
             pinch.cnt = 0;
+            pinch.evtMoveChk = false;
 
             setup();
             eventBox();
@@ -69,18 +80,16 @@
 
         // 클릭 시, 좌표값 생성
         var locationSetup = function(e) {
-            el.pageX = e.originalEvent.pageX - pinch.targetImg.offset().left;
-            el.pageY = e.originalEvent.pageY - pinch.targetImg.offset().top;
+            pinch.pageX = e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0].pageX : e.originalEvent.pageX;
+            pinch.pageY = e.originalEvent.changedTouches ? e.originalEvent.changedTouches[0].pageY : e.originalEvent.pageY;
+
+            el.pageX = pinch.pageX - pinch.targetItem.offset().left;
+            el.pageY = pinch.pageY - pinch.targetItem.offset().top;
         };
 
         // 클릭 시, 기준점 설정
         var benchmarkSetup = function(e) {
             locationSetup(e);
-
-            el.topLeft = pinch.imgWidth/2 > el.pageX && pinch.imgHeight/2 > el.pageY ? '0 0' : false;
-            el.topRight = pinch.imgWidth/2 < el.pageX && pinch.imgHeight/2 > el.pageY ? '100% 0' : false;
-            el.bottomLeft = pinch.imgWidth/2 > el.pageX && pinch.imgHeight/2 < el.pageY ? '0 100%' : false;
-            el.bottomRight = pinch.imgWidth/2 < el.pageX && pinch.imgHeight/2 < el.pageY ? '100% 100%' : false;
         }
 
         // pinch 이용 시, 모드 세팅
@@ -101,23 +110,22 @@
 
         var eventBox = function() {
             pinch.targetImg
-                .off(pinch.modeValue).on(pinch.modeValue, evtZoomValue)
-            
+                    .off(pinch.modeValue).on(pinch.modeValue, evtZoomValue)
+
             if (pinch.modeValue == 'mouseenter') {
                 pinch.container
                     .off('mouseleave').on('mouseleave', evtLeaveZoom)
-                    .off(pinch.evtTypeMove).on(pinch.evtTypeMove, evtMoveValue)
-            } else {
-                
-            }
+                    .off(pinch.evtTypeMove).on(pinch.evtTypeMove, evtMoveValue);
+            } 
         };
 
         var evtZoomValue = function(e) {
             locationSetup(e);
             benchmarkSetup(e);
-            
+
             pinch.cnt ++;
-            if (pinch.modeValue == 'click') {
+            
+            if (pinch.modeValue == pinch.evtTypeStart) {
                 if(pinch.cnt > 1) {clearTimeout(clickTimer);}
                 clickTimer = setTimeout(function(){
                     if (pinch.cnt > 1) {
@@ -127,15 +135,9 @@
                     pinch.cnt = 0;
                 }, 200)
                 
-            } else {evtEnterZoom(e);};
-
+            } else { evtEnterZoom(e); };
+            
             pinch.targetImg[0].style.transitionDuration = pinch.setting.transition_Duration + 's';
-
-            if (pinch.imgWidth/2 > el.pageX && pinch.imgHeight/2 > el.pageY) { pinch.targetImg[0].style.transformOrigin = el.topLeft; }
-            else if (pinch.imgWidth/2 < el.pageX && pinch.imgHeight/2 > el.pageY) { pinch.targetImg[0].style.transformOrigin = el.topRight; }
-            else if (pinch.imgWidth/2 > el.pageX && pinch.imgHeight/2 < el.pageY) { pinch.targetImg[0].style.transformOrigin = el.bottomLeft; }
-            else if (pinch.imgWidth/2 < el.pageX && pinch.imgHeight/2 < el.pageY) { pinch.targetImg[0].style.transformOrigin = el.bottomRight; };
-
         };
 
         var evtPinchZoom = function() {
@@ -149,55 +151,76 @@
 
         var pinch_on = function() {
             pinch.targetItem.addClass('pinch_on');
-            pinch.targetImg.css('transform', 'scale(' + pinch.setting.scaleValue + ')');
+            
+            pinch.targetImg.css({
+                'width': pinch.container.width() * pinch.setting.scaleValue,
+                'height': pinch.container.height() * pinch.setting.scaleValue,
+                'position': 'absolute',
+                'left': - el.pageX * (pinch.setting.scaleValue - 1),
+                'top': - el.pageY * (pinch.setting.scaleValue - 1)
+            });
 
             pinch.evtZoomChk = false;
+            if (!pinch.evtZoomChk) {pinch.targetImg.on(pinch.evtTypeMove, evtMoveValue);}
         };
 
         var pinch_off = function() {
-            pinch.targetImg.css('transform', 'scale(1)');
             pinch.targetItem.removeClass('pinch_on');
-
-            pinch.targetImg[0].style.left = 0;
-            pinch.targetImg[0].style.top = 0;
-        };
-
-        var evtEnterZoom = function() {
-            pinch.targetItem.addClass('hover_on');
-            console.log(el.pageX);
-            pinch.container.css({
-                'visibility': 'visible', 
-                'background-image': 'url(' + pinch.targetUrl + ')',
-                'transform': 'scale(' + pinch.setting.scaleValue + ')',
+            pinch.targetImg.css({
+                'width': pinch.container.width(),
+                'height': pinch.container.height(),
+                'position': 'relative',
                 'left': 0,
                 'top': 0
             });
+            
+            pinch.targetImg.off(pinch.evtTypeMove, evtMoveValue);
+        };
+
+        var evtEnterZoom = function(e) {
+            pinch.targetItem.addClass('hover_on');
+
+            console.log(pinch.modeValue)
+
+            pinch.container.css({
+                'visibility': 'visible', 
+                'background-image': 'url(' + pinch.targetUrl + ')',
+                'width': pinch.imgWidth * pinch.setting.scaleValue,
+                'height': pinch.imgHeight * pinch.setting.scaleValue,
+                'left': - el.pageX,
+                'top': - el.pageY
+            });
+
+            pinch.evtMoveChk = true;
+            if (pinch.evtMoveChk) {evtMoveValue(e);}
         };
 
         var evtLeaveZoom = function() {
-            console.log('mouseleave');
             pinch.targetItem.removeClass('hover_on');
+
+            pinch.evtMoveChk = false;
+
             pinch.container.css({
                 'visibility': '', 
                 'background-image': '',
-                'transform': 'scale(1)'
+                'width': pinch.imgWidth,
+                'height': pinch.imgHeight,
+                'left': 0,
+                'top': 0
             });
         };
         
         var evtMoveValue = function(e) {
             locationSetup(e);
 
-            
-            el.pageX = e.originalEvent.pageX;
-            el.pageY = e.originalEvent.pageY;
-
-            pinch.position_x = el.pageX - pinch.container.offset().left;
-            pinch.position_y = el.pageY - pinch.container.offset().top;
-            
-            // pinch.container.css({
-            //     'background-position-x': - pinch.position_x,
-            //     'background-position-y': - pinch.position_y
-            // });
+            pinch.obj = pinch.modeValue == 'mouseenter' ? pinch.container : pinch.targetImg;
+            el.pageX = pinch.modeValue == 'mouseenter' ? el.pageX : el.pageX * (pinch.setting.scaleValue - 1);
+            el.pageY = pinch.modeValue == 'mouseenter' ? el.pageY : el.pageY * (pinch.setting.scaleValue - 1);
+            pinch.targetImg[0].style.transitionDuration = 0 + 's';
+            pinch.obj.css({
+                'left': - el.pageX,
+                'top': - el.pageY
+            });
         };
 
         $(window).load(function(){init();})
